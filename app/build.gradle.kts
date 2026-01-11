b4b01e42aebd37addfde8ab7c01671303ca3bb1f
MarkGRX11 <marc11gandara@gmail.com>
2026-01-11 05:06:35 +0100

    Realtime Dashboard :)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.compose.foundation.layout)
     implementation(libs.androidx.foundation)
     implementation(libs.androidx.compose.animation.core)
+    implementation(libs.androidx.runtime)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
